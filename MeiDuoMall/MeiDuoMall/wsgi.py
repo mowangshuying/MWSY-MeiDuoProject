38e53299cd51c9edb4717518285af7a468cb9e0b
莫忘输赢 <1911364602@qq.com>
2021-12-30 13:46:57 +0800

    创建settings文件夹：文件夹下放入两个文件：dev.py,prod.py,分别为开发环境的配置文件和生产环境的配置文件，修改wsgi.py的项目配置文件为prod.py 修改manage.py项目配置文件为dev.py

--- MeiDuoMall/MeiDuoMall/wsgi.py
+++ MeiDuoMall/MeiDuoMall/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MeiDuoMall.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MeiDuoMall.settings.prod")
 
 application = get_wsgi_application()
