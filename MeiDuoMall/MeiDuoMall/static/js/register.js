d30211a84949fa6894131cb53678ba3cfbd5f8ed
莫忘输赢 <1911364602@qq.com>
2022-01-01 16:40:45 +0800

    1、添加register.js文件，绑定对象和方法 2、添加相应的说明文件

---
+++ MeiDuoMall/MeiDuoMall/static/js/register.js
@@ -0,0 +1,43 @@
+let vm = new Vue({
+    el:'#app',
+    data:{//数据对象
+        // v-model
+        // v-show
+        // err_message
+
+        username:'',
+        password:'',
+        password2:'',
+        mobile:'',
+        allow:'',
+
+        error_name:false,
+        error_password:false,
+        error_mobile:false,
+        error_allow:false,
+
+        error_name_message:'',
+        error_mobile_message:'',
+    },
+    methods:{//定义事件方法
+        check_username(){
+
+        },
+        check_password(){
+
+        },
+        check_password2(){
+
+        },
+        check_mobile(){
+
+        },
+        check_allow(){
+
+        },
+        on_submit(){
+
+        },
+    }
+});
+
