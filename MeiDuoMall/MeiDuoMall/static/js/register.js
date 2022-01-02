ed36205ee8a0367765356a822ae11750d575a0e8
莫忘输赢 <1911364602@qq.com>
2022-01-02 11:03:16 +0800

    对register.js及register.html，前端进行修改

--- MeiDuoMall/MeiDuoMall/static/js/register.js
+++ MeiDuoMall/MeiDuoMall/static/js/register.js
@@ -1,10 +1,8 @@
 let vm = new Vue({
     el:'#app',
-    data:{//数据对象
-        // v-model
-        // v-show
-        // err_message
-
+    delimiters:['[[',']]'],
+    data:{
+        //数据对象 // v-model // v-show // err_message
         username:'',
         password:'',
         password2:'',
@@ -21,22 +19,62 @@ let vm = new Vue({
     },
     methods:{//定义事件方法
         check_username(){
-
+            let re = /^[a-zA-Z0-9_-]{5,20}$/;
+            if(re.test(this.username)){
+                //匹配成功，不展示错误提示信息
+                this.error_name = false;
+            }else{
+                //匹配失败，展示错误提示信息
+                this.error_name_message = '请输入5-20个字符的用户名';
+                this.error_name = true;
+            }
         },
         check_password(){
-
+            let re = /^[0-9A-Za-z]{8,20}$/;
+            if (re.test(this.password)) {
+                this.error_password = false;
+            } else {
+                this.error_password = true;
+            }
         },
         check_password2(){
-
+            if (this.password != this.password2) {
+                this.error_password2 = true;
+            } else {
+                this.error_password2 = false;
+            }
         },
         check_mobile(){
-
+            let re = /^1[3-9]\d{9}$/;
+            if (re.test(this.mobile)) {
+                this.error_mobile = false;
+            } else {
+                this.error_mobile_message = '您输入的手机号格式不正确';
+                this.error_mobile = true;
+            }
         },
         check_allow(){
-
+            if (!this.allow) {
+                this.error_allow = true;
+            } else {
+                this.error_allow = false;
+            }
         },
         on_submit(){
+            this.check_username();
+            this.check_password();
+            this.check_password2();
+            this.check_mobile();
+            this.check_allow();
 
+            // 在校验之后，注册数据中，只要有错误，就禁用掉表单的提交事件
+            if ( this.error_name == true || this.error_password == true ||
+                 this.error_password2 == true || this.mobile == true || 
+                 this.allow == true) 
+           {
+                // 禁用掉表单的提交事件
+                window.event.returnValue = false;
+            }
         },
     }
 });
