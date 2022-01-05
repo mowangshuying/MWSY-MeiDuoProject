ed64659a671dfa4038ea03bdf806d111402e569b
莫忘输赢 <1911364602@qq.com>
2022-01-05 23:07:14 +0800

    添加验证码功能，向客户端返回特殊的图形验证码

--- MeiDuoMall/MeiDuoMall/apps/verifications/views.py
+++ MeiDuoMall/MeiDuoMall/apps/verifications/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
 from django.views import View
-# Create your views here.
 
+from django_redis import get_redis_connection
+from django import http
+from verifications.libs.captcha.captcha import captcha
+
+# 在此处创建你的视图
 class ImageCodeView(View):
     def get(self,request,uuid):
-        pass
+        # 接收和校验参数
+        # 生成图形验证码
+        text,image = captcha.generate_captcha()
+        print('text:',text,'image:',image)
+        # 保存图形验证码
+        redis_conn = get_redis_connection('virify_code')
+        #redis_conn.setex('key','expires','value')
+        redis_conn.setex('img_%s'%uuid,300,text)
+
+        return http.HttpResponse(image,content_type='image/jepg')
+
+
